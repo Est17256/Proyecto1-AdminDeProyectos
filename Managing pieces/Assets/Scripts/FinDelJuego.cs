f8d6b7354b20197c16dfc9761663642edcabf997
Raul Monzon <rmonzon1998@gmail.com>
2020-08-21 14:53:28 -0600

    Pantalla de fin de juego

---
+++ Managing pieces/Assets/Scripts/FinDelJuego.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinDelJuego : MonoBehaviour
+{
+    public bool GameIsFinished = false;
+    public GameObject EndMenuUI;
+    public Text titleText;
+    public Text scoreText;
+
+    // Update is called once per frame
+    public void ShowPanel(int score, bool victoria)
+    {
+        EndMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        if (victoria)
+        {
+            titleText.text = "Has ganado";
+        }
+        else
+        {
+            titleText.text = "Has perdido";
+        }
+        scoreText.text = score.ToString();
+    }
+
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

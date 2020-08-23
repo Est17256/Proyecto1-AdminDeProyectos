194d3485f475685128e1b825215fc773d3e7ed7b
Andres Urizar <andresum1309@gmail.com>
2020-08-22 22:34:44 -0600

    Plataformas para mundo y sprites para objetos
    Prefabs de plataformas moviles y tambien los sprites para los objetos

---
+++ Managing pieces/Assets/Scripts/Personaje.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("Plataforma")){
+            player.transform.parent = other.gameObject.transform;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other){
+        if(other.gameObject.CompareTag("Plataforma")){
+            player.transform.parent = null;
+        }
+    }
+}
